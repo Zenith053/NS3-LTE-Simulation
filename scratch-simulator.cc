edbe2325f567baecafc102658f640b01370eeede
Amandeep <100516949+amandeep2102@users.noreply.github.com>
2024-03-26 01:29:37 +0530

    Changes

--- scratch-simulator.cc
+++ scratch-simulator.cc
@@ -3,6 +3,9 @@
 #include <ns3/mobility-module.h>
 #include <ns3/lte-module.h>
 #include <ns3/config-store-module.h>
+#include <ns3/spectrum-module.h>
+
+//NS_LOG_COMPONENT_DEFINE("abcd");
 
 using namespace ns3;
 
@@ -14,46 +17,70 @@ int main(int argc, char *argv[]){
     inputConfig.ConfigureDefaults ();
 
     cmd.Parse (argc, argv);
+
+//    LogComponentEnable("abcd", LOG_LEVEL_INFO);
+
     Ptr<LteHelper> lteHelper = CreateObject<LteHelper>();
+//    Ptr<PointToPointEpcHelper> epcHelper = CreateObject<PointToPointEpcHelper>();
+//    lteHelper->SetEpcHelper(epcHelper);
 
     NodeContainer enbNodes;
     enbNodes.Create(4);
     NodeContainer ueNodes;
     ueNodes.Create(40);
 
+    //Setting up Frequency bands for uplink and downlink
+
+//    lteHelper->SetEnbDeviceAttribute("DlEarfcn", UintegerValue(100));
+//    lteHelper->SetEnbDeviceAttribute("UlEarfcn", UintegerValue(18100));
+
+    //configuring transmission power and uplink power control
+
+    Config::SetDefault("ns3::LteEnbPhy::TxPower", DoubleValue(40.0));
+//    Config::SetDefault("ns3::LteUePhy::TxPower", DoubleValue(20.0));
+//    Config::SetDefault("ns3::LteUePhy::EnableUplinkPowerControl", BooleanValue(true));
+
+    //configuring mimo
+
+//    Config::SetDefault("ns3::LteEnbRrc::DefaultTransmissionMode", UintegerValue(1));
+
+    //resource scheduling - round robin
+
+//    lteHelper->SetSchedulerType(("ns3::FdMtFfMacScheduler"));
+
+    //handover algorithms
+
+//    lteHelper->SetHandoverAlgorithmType("ns3::A2A4RsrqHandoverAlgorithm");
+//    lteHelper->SetHandoverAlgorithmAttribute("ServingCellThreshold", UintegerValue(1));
+//    lteHelper->SetHandoverAlgorithmAttribute("NeighbourCellOffset", UintegerValue(1));
+
+    //interference algorithms
+
+//    lteHelper->SetFfrAlgorithmType("ns3::LteFrHardAlgorithm");
+//
     //placing enodebs in a square topology
 
     MobilityHelper mobility;
     mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
 
-    //enodeb 0
+    //enodeb in topology
     Ptr<ListPositionAllocator> loc0 = CreateObject<ListPositionAllocator>();
-    loc0 -> Add(Vector(0,0,0));
-    loc0 -> Add(Vector(5000,0,0));
-    loc0 -> Add(Vector(5000,5000,0));
-    loc0 -> Add(Vector(0,5000,0));
+    loc0 -> Add(Vector(-2500,-2500,0));
+    loc0 -> Add(Vector(2500,-2500,0));
+    loc0 -> Add(Vector(2500,2500,0));
+    loc0 -> Add(Vector(-2500,2500,0));
     mobility.SetPositionAllocator(loc0);
     mobility.Install(enbNodes);
 
-//    //enodeb 1
-//    Ptr<ConstantPositionMobilityModel> loc1 = CreateObject<ConstantPositionMobilityModel>();
-//    loc1 ->SetPosition(Vector(5000, 0, 0));
-//    enbNodes.Get(1)->AggregateObject(loc1);
-//
-//    //enodeb 2
-//    Ptr<ConstantPositionMobilityModel> loc2 = CreateObject<ConstantPositionMobilityModel>();
-//    loc2 ->SetPosition(Vector(5000, 5000, 0));
-//    enbNodes.Get(2) ->AggregateObject(loc2);
-//
-//    //enodeb 3
-//    Ptr<ConstantPositionMobilityModel> loc3 = CreateObject<ConstantPositionMobilityModel>();
-//    loc3->SetPosition(Vector(0,5000,0));
-//    enbNodes.Get(3)->AggregateObject(loc3);
-
     //for uenodes defining stationary mobility model
     MobilityHelper mobility1;
-//    mobility1.SetMobilityModel("RandomWalk2D");
-    mobility.Install(ueNodes);
+    mobility1.SetMobilityModel("ns3::RandomWalk2dMobilityModel",
+                              "Bounds", RectangleValue(Rectangle(-2500, 2500, -2500, 2500)),
+                              "Time", StringValue("1s"),
+                              "Mode", StringValue("Time"),
+                              "Speed", StringValue("ns3::ConstantRandomVariable[Constant=10.0]"));
+
+    mobility1.Install(ueNodes);
 
     //installing lte protocol stack on eNBs
     NetDeviceContainer enbDevs;
@@ -88,26 +115,30 @@ int main(int argc, char *argv[]){
     EpsBearer bearer(q);
     lteHelper->ActivateDataRadioBearer(ueDevs, bearer);
 
+
     // configuring rem plot
     Ptr<RadioEnvironmentMapHelper> remHelper = CreateObject<RadioEnvironmentMapHelper>();
-    remHelper->SetAttribute("Channel", PointerValue(lteHelper->GetDownlinkSpectrumChannel()));
+//    remHelper->SetAttribute("Channel", PointerValue(lteHelper->GetDownlinkSpectrumChannel()));
+    remHelper->SetAttribute("ChannelPath", StringValue ("/ChannelList/0"));
     remHelper->SetAttribute("OutputFile", StringValue("rem.out"));
-    remHelper->SetAttribute("XMin", DoubleValue(-400.0));
-    remHelper->SetAttribute("XMax", DoubleValue(400.0));
-    remHelper->SetAttribute("XRes", UintegerValue(100));
-    remHelper->SetAttribute("YMin", DoubleValue(-300.0));
-    remHelper->SetAttribute("YMax", DoubleValue(300.0));
-    remHelper->SetAttribute("YRes", UintegerValue(75));
+    remHelper->SetAttribute("XMin", DoubleValue(-5000.0));
+    remHelper->SetAttribute("XMax", DoubleValue(5000.0));
+//    remHelper->SetAttribute("XRes", UintegerValue(100));
+    remHelper->SetAttribute("YMin", DoubleValue(-5000.0));
+    remHelper->SetAttribute("YMax", DoubleValue(5000.0));
+//    remHelper->SetAttribute("YRes", UintegerValue(75));
     remHelper->SetAttribute("Z", DoubleValue(0.0));
-    remHelper->SetAttribute("UseDataChannel", BooleanValue(true));
-    remHelper->SetAttribute("RbId", IntegerValue(10));
+//    remHelper->SetAttribute("UseDataChannel", BooleanValue(true));
+//    remHelper->SetAttribute("RbId", IntegerValue(10));
     remHelper->Install();
+
+    //Enable traces
 
 //    lteHelper->EnablePhyTraces();
 //    lteHelper->EnableMacTraces();
 //    lteHelper->EnableRlcTraces();
 //    lteHelper->EnablePdcpTraces();
-    lteHelper->EnableTraces();
+//    lteHelper->EnableTraces();
 
     Simulator::Stop(Seconds(0.005));
     Simulator::Run();
